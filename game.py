20e39e0ccc6e16349c8e3d6fe87c395ecd18f0f9
Saraev Vyacheslav <saraev-slava@yandex.ru>
2025-10-23 23:52:31 +0300

    23.10 version

--- game.py
+++ game.py
@@ -1,7 +1,12 @@
-from parts import Board
+from gameparts import Board
 
-game = Board()
-game.display()
-game.make_move(1, 1, 'X')
-print('Ход сделан!')
-game.display() de
+def main():
+    game = Board()
+    game.display()
+    game.make_move(1, 1, 'X')
+    print('Ход сделан!')
+    game.make_move(0, 2, 'O')
+    game.display() 
+
+if __name__ == '__main__':
+    main()
